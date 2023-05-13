618c8e6fd0727da7e10a19e6224c8cdeb280d2d1
Hailey Somerville <hailey@hailey.lol>
2023-05-13 16:29:25 +1000

    make the entire crate vc6-sys

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,6 @@
 #![no_std]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
 
-#[allow(non_camel_case_types)]
-pub mod c_types {
-    pub type c_uint = ::core::ffi::c_uint;
-    pub type c_int = ::core::ffi::c_int;
-    pub type c_void = ::core::ffi::c_void;
-}
-
-#[allow(non_snake_case)]
-#[allow(non_upper_case_globals)]
-#[allow(non_camel_case_types)]
-pub mod sys {
-    include!("bindings.rs");
-}
+include!("bindings.rs");
